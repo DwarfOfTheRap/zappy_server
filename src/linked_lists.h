fff1524dd8ed20bf7eff38294e848d74b35459cc
Julien LINDEN <jlinden@student.42.fr>
2015-12-14 19:33:04 +0100

    Add lst_map

--- src/linked_lists.h
+++ src/linked_lists.h
@@ -1,16 +1,18 @@
 #ifndef LINKED_LISTS_H
 # define LINKED_LISTS_H
 
+# include <stdlib.h>
+
 typedef struct	s_lst_elem
 {
-	void			*content;
-	t_lst_elem		*next;
-	t_lst_elem		*prev;
+	void					*content;
+	struct s_lst_elem		*next;
+	struct s_lst_elem		*prev;
 }				t_lst_elem;
 
 typedef struct	s_lst_head
 {
-	t_lst_elem		*first;	
+	t_lst_elem		*first;
 	t_lst_elem		*last;
 }				t_lst_head;
 
@@ -18,5 +20,7 @@ t_lst_head		*lst_init(t_lst_elem *first, t_lst_elem *last);
 t_lst_elem		*lst_create(void *content, size_t content_size);
 void			lst_pushfront(t_lst_head *head, t_lst_elem *new);
 void			lst_pushback(t_lst_head *head, t_lst_elem *new);
+t_lst_head		*lst_map(t_lst_head *hd, t_lst_elem *(*f)(t_lst_elem *e) \
+																, size_t s);
 
 #endif
