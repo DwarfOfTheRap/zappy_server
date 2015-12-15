f18556a71681aa178ca1b825b0a7e39b02f4751c
Julien LINDEN <jlinden@student.42.fr>
2015-12-15 13:16:00 +0100

    Add lst_remove, lst_pop, lst_delete, lst_delete_elem, lst_free_match functions

--- src/linked_lists.h
+++ src/linked_lists.h
@@ -24,5 +24,11 @@ void			lst_pushback(t_lst_head *head, t_lst_elem *new);
 t_lst_head		*lst_map(t_lst_head *hd, t_lst_elem *(*f)(t_lst_elem *e) \
 																, size_t s);
 t_lst_elem		*lst_pop(t_lst_head *head, unsigned int index_to_remove);
+t_lst_elem		*lst_remove(t_lst_head *head, t_lst_elem *elem_to_remove);
+void			lst_delete_elem(t_lst_elem **elem, void (*del)(void*));
+void			lst_free_match(t_lst_head *hd, void *dt \
+							, int *(*cmp)(void*, void*), void (*del)(void*));
+void			lst_delete(t_lst_head *head, void (*del)(void *));
+
 
 #endif
--- src/linked_lists_2.c
+++ src/linked_lists_2.c
@@ -1,7 +1,25 @@
 #include <stdlib.h>
 #include "linked_lists.h"
 
-t_lst_elem		*lst_pop(t_lst_head *head, unsigned int index_to_remove)
+t_lst_elem	*lst_remove(t_lst_head *head, t_lst_elem *elem_to_remove)
+{
+	if (!(head && elem_to_remove))
+		return (NULL);
+	if (!elem_to_remove->prev)
+		head->first = elem_to_remove->next;
+	else
+		elem_to_remove->prev->next = elem_to_remove->next;
+	if (!elem_to_remove->next)
+		head->last = elem_to_remove->prev;
+	else
+		elem_to_remove->next->prev = elem_to_remove->prev;
+	elem_to_remove->next = NULL;
+	elem_to_remove->prev = NULL;
+	head->size--;
+	return (elem_to_remove);
+}
+
+t_lst_elem	*lst_pop(t_lst_head *head, unsigned int index_to_remove)
 {
 	t_lst_elem	*cursor;
 	t_lst_elem	*result;
@@ -12,18 +30,53 @@ t_lst_elem		*lst_pop(t_lst_head *head, unsigned int index_to_remove)
 	while (cursor && index_to_remove--)
 		cursor = cursor->next;
 	if (cursor)
-	{
-		if (!cursor->prev)
-			head->first = cursor->next;
-		else
-			cursor->prev->next = cursor->next;
-		if (!cursor->next)
-			head->last = cursor->prev;
-		else
-			cursor->next->prev = cursor->prev;
-		cursor->next = NULL;
-		cursor->prev = NULL;
-		head->size--;
-	}
+		cursor = lst_remove(head, cursor);
 	return (cursor);
+}
+
+void		lst_delete_elem(t_lst_elem **elem, void (*del)(void*))
+{
+	if (elem)
+	{
+		del((*elem)->content);
+		free(*elem);
+		*elem = NULL;
+	}
+}
+
+void		lst_free_match(t_lst_head *hd, void *dt
+								, int *(*cmp)(void*, void*), void (*del)(void*))
+{
+	t_lst_elem	*cursor;
+	t_lst_elem	*elem_to_remove;
+
+	elem_to_remove = NULL;
+	if (!hd)
+		return ;
+	cursor = hd->first;
+	while (cursor)
+	{
+		if (cmp(cursor->content, dt))
+		{
+			elem_to_remove = lst_remove(hd, cursor);
+			lst_delete_elem(&elem_to_remove, del);
+		}
+		cursor = cursor->next;
+	}
+}
+
+void		lst_delete(t_lst_head *head, void (*del)(void*))
+{
+	t_lst_elem	*cursor;
+	t_lst_elem	*elem_to_remove;
+
+	if (!head)
+		return ;
+	cursor = head->first;
+	while (cursor)
+	{
+		elem_to_remove = lst_remove(head, cursor);
+		lst_delete_elem(&elem_to_remove, del);
+		cursor = cursor->next;
+	}
 }
