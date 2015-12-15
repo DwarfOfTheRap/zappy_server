ad9995ed5ea9af14fec6690d37bbbf9ada1a886b
Julien LINDEN <jlinden@student.42.fr>
2015-12-15 16:00:22 +0100

    Fix forgotten semi-colon in linked_lists.h

--- src/linked_lists.h
+++ src/linked_lists.h
@@ -29,6 +29,6 @@ void			lst_delete_elem(t_lst_elem **elem, void (*del)(void*));
 void			lst_free_match(t_lst_head *hd, void *dt \
 							, int *(*cmp)(void*, void*), void (*del)(void*));
 void			lst_delete(t_lst_head *head, void (*del)(void *));
-void			lst_insert(t_lst_head *head, t_lst_elem *new, int (*f)(void*))
+void			lst_insert(t_lst_head *head, t_lst_elem *new, int (*f)(void*));
 
 #endif
