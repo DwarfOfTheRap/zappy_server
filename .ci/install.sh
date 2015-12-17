2cd58217a50c7755869b087f95f219711b212ff1
Julien LINDEN <jlinden@student.42.fr>
2015-12-17 18:56:29 +0100

    Try to fix travis linux

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,4 +2,12 @@
 
 if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
     brew install check cmake
+elif [[ "${TRAVIS_OS_NAME}" == linux ]]; then
+	wget 'http://downloads.sourceforge.net/project/check/check/0.10.0/check-0.10.0.tar.gz'
+	tar xzf 'check-0.10.0.tar.gz'
+	cd 'check-0.10.0'
+	./configure
+	make
+	make check
+	sudo make install
 fi
