293b3917fa047af3d316bd6b23bd3f88ad6a1ede
Julien LINDEN <jlinden@student.42.fr>
2015-12-17 19:20:19 +0100

    new attempt with travis on for linux

--- .ci/install.sh
+++ .ci/install.sh
@@ -3,10 +3,12 @@
 if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
     brew install check cmake
 elif [[ "${TRAVIS_OS_NAME}" == linux ]]; then
-	wget 'http://downloads.sourceforge.net/project/check/check/0.10.0/check-0.10.0.tar.gz'
-	tar xzf 'check-0.10.0.tar.gz'
-	cd 'check-0.10.0'
-	./configure
-	make
-	sudo make install
+	sudo apt-get update
+	sudo apt-get install check
+	#wget 'http://downloads.sourceforge.net/project/check/check/0.10.0/check-0.10.0.tar.gz'
+	#tar xzf 'check-0.10.0.tar.gz'
+	#cd 'check-0.10.0'
+	#./configure
+	#make
+	#sudo make install
 fi
