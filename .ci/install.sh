f02300e0a407cb2a190009bac0bb30cd9f925920
Julien LINDEN <jlinden@student.42.fr>
2015-12-17 19:34:25 +0100

    Fix travis linux for good

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,10 +5,4 @@ if [[ "${TRAVIS_OS_NAME}" == osx ]]; then
 elif [[ "${TRAVIS_OS_NAME}" == linux ]]; then
 	sudo apt-get update
 	sudo apt-get install check
-	#wget 'http://downloads.sourceforge.net/project/check/check/0.10.0/check-0.10.0.tar.gz'
-	#tar xzf 'check-0.10.0.tar.gz'
-	#cd 'check-0.10.0'
-	#./configure
-	#make
-	#sudo make install
 fi
