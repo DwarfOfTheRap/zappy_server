65f97339288a53b3fd91ebf49b24b28a104c9545
Julien LINDEN <jlinden@student.42.fr>
2015-12-18 17:00:19 +0100

    Fix header file in linked lists tests

--- test/src/l_lists/linked_lists.h
+++ test/src/l_lists/linked_lists.h
@@ -1,1 +1,34 @@
-/nfs/zfs-student-2/users/jlinden/Zappy/zappy_server/src/linked_lists.h
+#ifndef LINKED_LISTS_H
+# define LINKED_LISTS_H
+
+# include <stdlib.h>
+
+typedef struct	s_lst_elem
+{
+	void					*content;
+	struct s_lst_elem		*next;
+	struct s_lst_elem		*prev;
+}				t_lst_elem;
+
+typedef struct	s_lst_head
+{
+	t_lst_elem		*first;
+	t_lst_elem		*last;
+	size_t			size;
+}				t_lst_head;
+
+t_lst_head		*lst_init(t_lst_elem *first);
+t_lst_elem		*lst_create(void *content, size_t content_size);
+void			lst_pushfront(t_lst_head *head, t_lst_elem *new);
+void			lst_pushback(t_lst_head *head, t_lst_elem *new);
+t_lst_head		*lst_map(t_lst_head *hd, t_lst_elem *(*f)(t_lst_elem *e) \
+																, size_t s);
+t_lst_elem		*lst_pop(t_lst_head *head, unsigned int index_to_remove);
+t_lst_elem		*lst_remove(t_lst_head *head, t_lst_elem *elem_to_remove);
+void			lst_delete_elem(t_lst_elem **elem, void (*del)(void*));
+void			lst_free_match(t_lst_head *hd, void *dt \
+							, int (*cmp)(void*, void*), void (*del)(void*));
+void			lst_delete(t_lst_head *head, void (*del)(void *));
+void			lst_insert(t_lst_head *head, t_lst_elem *new, int (*f)(void*));
+
+#endif
