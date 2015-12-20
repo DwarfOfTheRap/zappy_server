edfdb6c1f903c8ed0563b51ae791e5b5ae2b3eb1
Marc DELAGE <mdelage@student.42.fr>
2015-12-20 22:47:46 +0100

    classical, forget the files..

---
+++ test/src/init/init_game_var.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <check.h>
+#include "serveur.h"
+
+START_TEST(init_game_var_valid)
+{
+	int					ac = 14;
+	char				*av[14] = {"./test", "-p", "1234", "-x", "20", "-y",
+		"30", "-n", "toto", "tutu", "-c", "40", "-t", "50"};
+	t_zappy		var;
+	t_arguments	args;
+	t_server	serv;
+
+	bzero(&serv, sizeof(t_server));
+	bzero(&args, sizeof(t_arguments));
+	read_arguments(ac, (const char **)av, &args);
+	ck_assert_int_eq(0, init_game_var(&var, &args));
+	ck_assert_ptr_ne(NULL, var.board);
+	ck_assert_ptr_eq(NULL, var.actions);
+	ck_assert_ptr_eq(args.teams, var.teams);
+	ck_assert_int_eq(args.nb_team, var.nb_team);
+	ck_assert_int_eq(args.height, var.board_size[0]);
+	ck_assert_int_eq(args.width, var.board_size[1]);
+	ck_assert_int_eq(args.nb_clients / (int)args.nb_team, var.team_size);
+	ck_assert_int_eq(args.tick, var.tick);
+	cleanup_game(&var, &serv);
+}
+END_TEST
+
+TCase*	init_init_game_var(void)
+{
+	TCase	*tc;
+
+	tc = tcase_create("init_game_var");
+	tcase_add_test(tc, init_game_var_valid);
+	return (tc);
+}
---
+++ test/src/init/init_test.h
@@ -0,0 +1,7 @@
+#ifndef INIT_TEST_H
+# define INIT_TEST_H
+
+TCase*	init_init_game_var(void);
+TCase*	init_init_server(void);
+
+#endif
