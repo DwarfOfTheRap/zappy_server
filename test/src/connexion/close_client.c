0c2a1ef99589ade7c2cebd450dbccffc792fb343
Marc DELAGE <mdelage@student.42.fr>
2016-02-08 14:52:03 +0100

    place all dummy generator in same file

--- test/src/connexion/close_client.c
+++ test/src/connexion/close_client.c
@@ -1,44 +1,7 @@
 #include <stdlib.h>
 #include <check.h>
 #include "serveur.h"
-
-void	connexion_dummy_t_zappy(t_zappy *var)
-{
-	var->board = (int ***)0xdeadbeaf;
-	var->board_size[0] = 1;
-	var->board_size[1] = 2;
-	var->team_size = 3;
-	var->tick = 4;
-	var->nb_team = 3;
-	var->teams = (t_team *)malloc(sizeof(t_team) * 3);
-	bzero(var->teams, sizeof(t_team) * 3);
-	var->teams[0].name = strdup("toto");
-	var->teams[0].remain = 0;
-	var->teams[1].name = strdup("tutu");
-	var->teams[1].remain = 0;
-	var->teams[2].name = strdup("GRAPHIC");
-	var->teams[2].remain = 0;
-	bzero(&var->actions.size, sizeof(t_lst_head));
-}
-
-void	connexion_dummy_t_serv(t_server *serv)
-{
-	serv->sock = 5;
-	serv->port = 6;
-	serv->fd_max = 7;
-	serv->fd_sel = 8;
-}
-
-void	connexion_dummy_t_player(t_zappy *var, t_player *p)
-{
-	p->status = FD_CLIENT;
-	p->snd.lst.size = 1;
-	p->snd.lst.first = lst_create("caca", 4);
-	p->snd.lst.last = p->snd.lst.first;
-	p->team = &(var->teams[0]);
-	p->rcv.remain = (char *)malloc(16);
-	strcpy(p->rcv.remain, "caca");
-}
+#include "test_dummies.h"
 
 START_TEST(connexion_close_client_)
 {
---
+++ test/src/test_dummies.h
@@ -0,0 +1,9 @@
+#ifndef TEST_DUMMIES_H
+#define TEST_DUMMIES_H
+#include "serveur.h"
+
+void	dummy_t_zappy_without_board(t_zappy *var);
+void	dummy_t_serv(t_server *serv);
+void	dummy_t_player(t_zappy *var, t_player *p);
+
+#endif
