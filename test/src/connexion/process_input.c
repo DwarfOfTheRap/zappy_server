3ea22877a8009b1ae5bc46f4537382255f0c738c
Marc DELAGE <mdelage@student.42.fr>
2016-02-15 14:07:41 +0100

    correct process_input test and stabilise commit message after rush before taking train

--- test/src/connexion/process_input.c
+++ test/src/connexion/process_input.c
@@ -1,4 +1,5 @@
 #include <check.h>
+#include <stdio.h>
 #include "serveur.h"
 #include "test_dummies.h"
 
@@ -7,14 +8,16 @@ START_TEST(connexion_process_input_)
 	t_zappy		var;
 	t_player	*p = &var.players[5];
 	char		str[10] = "toto\n";
+	char		str2[128];
 
 	dummy_t_zappy_without_board(&var);
 	dummy_t_zappy_add_remaining_in_team(&var);
 	dummy_t_player(&var, p);
+	sprintf(str2, "%d\n%d %d\n", p->team->remain, var.board_size[1], var.board_size[0]);
 	p->status = FD_USED;
 	p->team = NULL;
 	process_input(&var, p, str);
-	ck_assert_str_eq(p->snd.buf[p->snd.read], "");
+	ck_assert_str_eq(p->snd.buf[p->snd.read], str2);
 	ck_assert_ptr_eq(p->team, &var.teams[0]);
 }
 END_TEST
