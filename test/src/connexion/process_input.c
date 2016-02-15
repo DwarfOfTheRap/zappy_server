439e0ee9b1fb0f78a901ecf4e06bd389dd82cca5
Marc DELAGE <mdelage@student.42.fr>
2016-02-15 14:30:47 +0100

    add start of gfx client initialisation test

--- test/src/connexion/process_input.c
+++ test/src/connexion/process_input.c
@@ -3,7 +3,7 @@
 #include "serveur.h"
 #include "test_dummies.h"
 
-START_TEST(connexion_process_input_)
+START_TEST(connexion_process_input_normal)
 {
 	t_zappy		var;
 	t_player	*p = &var.players[5];
@@ -22,11 +22,33 @@ START_TEST(connexion_process_input_)
 }
 END_TEST
 
+START_TEST(connexion_process_input_gfx)
+{
+	t_zappy		var;
+	t_player	*p = &var.players[5];
+	char		str[10] = "GRAPHIC\n";
+	char		str2[128] = "";
+
+	dummy_t_zappy_without_board(&var);
+	dummy_t_zappy_add_remaining_in_team(&var);
+	dummy_t_player(&var, p);
+	p->status = FD_USED;
+	p->team = NULL;
+	process_input(&var, p, str);
+	// this test need to be completed when we would have a proper gfx client
+	// initialisation management. Though we won't be able to do advance test due
+	// to random food location on map.
+	ck_assert_str_eq(p->snd.buf[p->snd.read], str2);
+	ck_assert_ptr_eq(p->team, &var.teams[var.nb_team - 1]);
+}
+END_TEST
+
 TCase*	connexion_process_input(void)
 {
 	TCase	*tc;
 
 	tc = tcase_create("process_input");
-	tcase_add_test(tc, connexion_process_input_);
+	tcase_add_test(tc, connexion_process_input_normal);
+	tcase_add_test(tc, connexion_process_input_gfx);
 	return (tc);
 }
