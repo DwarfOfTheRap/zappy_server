c1dda8809717cbea2bfd17ed54b27423b714610d
Marc DELAGE <mdelage@student.42.fr>
2016-02-27 16:05:11 +0100

    remove this test cause it has no sens since the command is implement (it's the command that cause the segfault cause it's not fully implement)

--- test/src/commands/find_command.c
+++ test/src/commands/find_command.c
@@ -79,22 +79,6 @@ START_TEST(commands_find_command_unsupported_command)
 }
 END_TEST
 
-START_TEST(commands_find_command_unsupported_command_gfx)
-{
-	t_zappy		var;
-	t_player	*p = &var.players[5];
-	char		str[10] = "sgt\n";
-
-	dummy_t_zappy_without_board(&var);
-	dummy_t_player(&var, p);
-	p->status = FD_GFX;
-	find_command(&var, p, str, 3);
-	ck_assert_str_eq(p->snd.buf[p->snd.read], "suc\n");
-	clean_msg_queue(p);
-	rm_teams(&var.teams, &var.nb_team);
-}
-END_TEST
-
 //START_TEST(commands_find_command_unauthorised_command)
 //{
 //	t_zappy		var;
@@ -137,7 +121,6 @@ TCase*	commands_find_command(void)
 	tcase_add_test(tc, commands_find_command_command_format_error_gfx);
 	// those tests are supposed to be remove when the command will be coded
 	tcase_add_test(tc, commands_find_command_unsupported_command);
-	tcase_add_test(tc, commands_find_command_unsupported_command_gfx);
 	// those tests are supposed to be uncomment when command will be coded
 	//tcase_add_test(tc, commands_find_command_unauthorised_command);
 	//tcase_add_test(tc, commands_find_command_unauthorised_command_gfx);
