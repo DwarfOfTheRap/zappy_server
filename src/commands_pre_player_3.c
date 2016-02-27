d29dc71b71ceedccbd0ee887d20cd4b1f8757618
Marc DELAGE <mdelage@student.42.fr>
2016-02-27 18:30:19 +0100

    add a first draft of commande_pre_player

---
+++ src/commands_pre_player_3.c
@@ -0,0 +1,16 @@
+#include "serveur.h"
+
+void	command_pre_fork(t_zappy *var, t_player *p, char *args)
+{
+	(void)var;
+	(void)args;
+	// send message to gfx_client
+	// add player action to action queue
+}
+
+void	command_pre_connect_nbr(t_zappy *var, t_player *p, char *args)
+{
+	(void)args;
+	(void)var;
+	message_player_connect_nbr(p);
+}
