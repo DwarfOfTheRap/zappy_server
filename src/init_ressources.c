4372f8980a0ab8aa79cd1036c5b4c25e95643e5d
Marc DELAGE <mdelage@student.42.fr>
2016-03-04 18:03:36 +0100

    add other ressource management functions

--- src/init_ressources.c
+++ src/init_ressources.c
@@ -1,14 +1,19 @@
 #include "serveur.h"
 
-void	add_ressource(t_zappy *var, int res, int nb)
+void	add_ressource_on_random_square(t_zappy *var, int res)
 {
 	int		pos[2];
 
+	pos[0] = rand() % var->board_size[0];
+	pos[1] = rand() % var->board_size[1];
+	++var->board[pos[0]][pos[1]][res];
+}
+
+void	add_ressource(t_zappy *var, int res, int nb)
+{
 	while (nb)
 	{
-		pos[0] = rand() % var->board_size[0];
-		pos[1] = rand() % var->board_size[1];
-		++var->board[pos[0]][pos[1]][res];
+		add_ressource_on_random_square(var, res);
 		--nb;
 	}
 }
@@ -25,4 +30,24 @@ void	init_ressources(t_zappy *var)
 	add_ressource(var, 4, nb_square * RES_RATIO / 100 * 5 / 39);
 	add_ressource(var, 5, nb_square * RES_RATIO / 100 * 6 / 39);
 	add_ressource(var, 6, nb_square * RES_RATIO / 100 / 39);
+}
+
+void	dispatch_incantation_ressources(t_zappy *var, t_player *p, int *res)
+{
+	int		i;
+	int		j;
+	int		*square;
+
+	i = 1;
+	square = var->board[p->coord[0]][p->coord[1]];
+	while (i < 7)
+	{
+		j = res[i];
+		while (j && square[i])
+		{
+			add_ressource_on_random_square(var, i);
+			--square[i];
+			--j;
+		}
+	}
 }
