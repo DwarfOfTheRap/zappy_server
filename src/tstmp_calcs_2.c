3dd883e70e6acd11276d464bc1329d5cf93380cb
Julien LINDEN <jlinden@student.42.fr>
2016-03-10 18:46:37 +0100

    Fix time_sub and time_elapsed pointers

--- src/tstmp_calcs_2.c
+++ src/tstmp_calcs_2.c
@@ -5,11 +5,11 @@ t_tstmp		time_sub(t_tstmp *time1, t_tstmp *time2)
 {
 	double		microseconds;
 
-	microseconds = (time2.tv_sec - time.tv_sec) * 1000000 + ((int)time2.tv_usec - (int)time.tv_usec);
+	microseconds = (time2->tv_sec - time->tv_sec) * 1000000 + ((int)time2->tv_usec - (int)time->tv_usec);
 	return (time_create(microseconds));
 }
 
 double		time_elapsed(t_tstmp *time1, t_tstmp *time2)
 {
-	return ((time2.tv_sec - time.tv_sec) * 1000000 + ((int)time2.tv_usec - (int)time.tv_usec));
+	return ((time2->tv_sec - time->tv_sec) * 1000000 + ((int)time2->tv_usec - (int)time->tv_usec));
 }
