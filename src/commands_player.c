4a94ed81c082053b443ec7d2ab88779be9e812f4
Marc DELAGE <mdelage@student.42.fr>
2016-03-10 22:43:25 +0100

    send updated info on move

--- src/commands_player.c
+++ src/commands_player.c
@@ -13,6 +13,7 @@ void	command_avance(t_zappy *var, t_player *p, char *args)
 	bzero(&t, sizeof(t_aargs));
 	action_add_wrapper(var, p, &t, AVANCE);
 	memcpy(&dummy, p, sizeof(t_player) - sizeof(t_rcv_buf) - sizeof(t_snd_buf));
+	action_player_move(var, &dummy, dummy.facing);
 	message_gfx_ppo(var, &dummy);
 	if (g_log & LOG_C)
 		printf("[\033[0;32mCOMMAND\033[0m] p %d -> avance\n", p->id);
@@ -27,7 +28,8 @@ void	command_droite(t_zappy *var, t_player *p, char *args)
 	bzero(&t, sizeof(t_aargs));
 	action_add_wrapper(var, p, &t, DROITE);
 	memcpy(&dummy, p, sizeof(t_player) - sizeof(t_rcv_buf) - sizeof(t_snd_buf));
-	message_gfx_ppo(var, p);
+	dummy.facing = (dummy.facing + 1 == 4) ? 0 : dummy.facing + 1;
+	message_gfx_ppo(var, &dummy);
 	if (g_log & LOG_C)
 		printf("[\033[0;32mCOMMAND\033[0m] p %d -> droite\n", p->id);
 }
@@ -41,7 +43,8 @@ void	command_gauche(t_zappy *var, t_player *p, char *args)
 	bzero(&t, sizeof(t_aargs));
 	action_add_wrapper(var, p, &t, GAUCHE);
 	memcpy(&dummy, p, sizeof(t_player) - sizeof(t_rcv_buf) - sizeof(t_snd_buf));
-	message_gfx_ppo(var, p);
+	dummy.facing = (dummy.facing - 1 < 0) ? 3 : dummy.facing - 1;
+	message_gfx_ppo(var, &dummy);
 	if (g_log & LOG_C)
 		printf("[\033[0;32mCOMMAND\033[0m] p %d -> gauche\n", p->id);
 }
