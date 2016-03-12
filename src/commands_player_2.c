298c1226df3d8f15e49f3ad7341095fdb99c9182
Marc DELAGE <mdelage@student.42.fr>
2016-03-12 18:35:54 +0100

    quick commit before merge

--- src/commands_player_2.c
+++ src/commands_player_2.c
@@ -8,11 +8,15 @@ extern const char	g_ressources[7][16];
 void	command_prend(t_zappy *var, t_player *p, char *args)
 {
 	t_aargs		t;
+	int		i;
 
+	i = 0;
+	while (i < 7 && strcmp(g_ressources[i], args))
+		++i;
 	bzero(&t, sizeof(t_aargs));
 	t.str = strdup(args);
 	action_add_wrapper(var, p, &t, PREND);
-	message_gfx_pgt(var, p, 0);
+	message_gfx_pgt(var, p, (i < 7) ? i : 0);
 	if (g_log & LOG_C)
 		printf("[\033[0;32mCOMMAND\033[0m] p %d -> prend %s\n", p->id, args);
 }
@@ -20,11 +24,15 @@ void	command_prend(t_zappy *var, t_player *p, char *args)
 void	command_pose(t_zappy *var, t_player *p, char *args)
 {
 	t_aargs		t;
+	int		i;
 
+	i = 0;
+	while (i < 7 && strcmp(g_ressources[i], args))
+		++i;
 	bzero(&t, sizeof(t_aargs));
 	t.str = strdup(args);
 	action_add_wrapper(var, p, &t, POSE);
-	message_gfx_pdr(var, p, 0);
+	message_gfx_pdr(var, p, (i < 7) ? i : 0);
 	if (g_log & LOG_C)
 		printf("[\033[0;32mCOMMAND\033[0m] p %d -> pose %s\n", p->id, args);
 }
--- test/src/commands/commands_player_voir.c
+++ test/src/commands/commands_player_voir.c
@@ -31,43 +31,37 @@ START_TEST(command_player_inventaire_test)
 	t_action	*action;
 
 	dummy_t_zappy_without_board(&var);
-	var.board_size[0] = 10;
-	var.board_size[1] = 10;
-	dummy_t_zappy_add_board(&var);
 	dummy_t_player(&var, p);
 	command_inventaire(&var, p, NULL);
 	action = p->actions->first->content;
 	ck_assert_ptr_eq(action->run, &action_player_inventaire);
 	clean_msg_queue(p);
-	action_player_clear(p, &var);
-	rm_board(&var.board, var.board_size, var.board_size[0], var.board_size[1]);
+	//action_player_clear(p, &var);
 	rm_teams(&var.teams, &var.nb_team);
 }
 END_TEST
 
-//START_TEST(command_player_prend_test)
-//{
-//	t_zappy		var;
-//	t_player	*gfx = &var.players[5];
-//	t_player	*p = &var.players[6];
-//	t_action	*action;
-//
-//	dummy_t_zappy_without_board(&var);
-//	var.board_size[0] = 10;
-//	var.board_size[1] = 10;
-//	dummy_t_zappy_add_board(&var);
-//	dummy_t_player_gfx(&var, gfx);
-//	dummy_t_player(&var, p);
-//	command_prend(&var, p, );
-//	action = p->actions->first->content;
-//	ck_assert_ptr_eq(action->run, &action_player_prend);
-//	clean_msg_queue(gfx);
-//	clean_msg_queue(p);
-//	//action_player_clear(p, &var);
-//	rm_board(&var.board, var.board_size, var.board_size[0], var.board_size[1]);
-//	rm_teams(&var.teams, &var.nb_team);
-//}
-//END_TEST
+START_TEST(command_player_prend_test)
+{
+	t_zappy		var;
+	t_player	*gfx = &var.players[5];
+	t_player	*p = &var.players[6];
+	t_action	*action;
+
+	dummy_t_zappy_without_board(&var);
+	dummy_t_zappy_add_board(&var);
+	dummy_t_player_gfx(&var, gfx);
+	dummy_t_player(&var, p);
+	command_prend(&var, p, "linemate");
+	action = p->actions->first->content;
+	ck_assert_ptr_eq(action->run, &action_player_prend);
+	clean_msg_queue(gfx);
+	clean_msg_queue(p);
+	//action_player_clear(p, &var);
+	rm_board(&var.board, var.board_size, var.board_size[0], var.board_size[1]);
+	rm_teams(&var.teams, &var.nb_team);
+}
+END_TEST
 
 TCase*	commands_player_ressources_test(void)
 {
@@ -76,5 +70,6 @@ TCase*	commands_player_ressources_test(void)
 	tc = tcase_create("player_voir");
 	tcase_add_test(tc, command_player_voir_test);
 	tcase_add_test(tc, command_player_inventaire_test);
+	tcase_add_test(tc, command_player_prend_test);
 	return (tc);
 }
