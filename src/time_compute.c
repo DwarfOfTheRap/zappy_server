624a97ffd2b2231f6467e495f6707b95a5992684
Julien LINDEN <jlinden@student.42.fr>
2016-03-13 20:12:05 +0100

    Complete update_tick function - still untested

--- src/time_compute.c
+++ src/time_compute.c
@@ -1,16 +1,51 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include "serveur.h"
 
-void	compute_action_new_time(t_action *action, int old_tick, t_zappy *var)
+extern const t_action_d	g_action[12];
+extern int				g_log;
+
+static int	action_find_ref(t_action *action)
 {
-	(void)action;
-	(void)old_tick;
-	(void)var;
+	int	i;
+
+	i = 0;
+	while (i < 12)
+	{
+		if (g_action[i].f == action->run)
+			return (g_action[i].rel_time);
+		i++;
+	}
+	return (-1);
 }
 
-void	compute_death_new_time(t_player *p, int old_tick, t_zappy *var)
+void		compute_action_new_time(t_action *action, t_zappy *var)
 {
-	(void)p;
-	(void)old_tick;
-	(void)var;
+	double	old_start;
+	double	old_end;
+	double	current;
+	int		ref;
+	float	ratio;
+
+	old_start = time_double(action->creation_t);
+	old_end = time_double(action->trigger_t);
+	current = time_double(var->start_time);
+	ratio = (1 - (current - old_start) / (old_end - old_start));
+	if ((ref = action_find_ref(action)) < 0)
+	{
+		if (g_log & LOG_E)
+			printf("[\033[0;31mERROR\033[0m] update_tick can't find ref time\n");
+		return ;
+	}
+	old_start = (ref / (double)var->tick) * 1000000 * ratio;
+	action->trigger_t = time_create(current + old_start);
+	action->creation_t = var->start_time;
+}
+
+void		compute_death_new_time(t_player *p, int old_tick, t_zappy *var)
+{
+	double	death_ref;
+
+	death_ref = time_elapsed(var->start_time, p->timeofdeath) / 1000000 * old_tick;
+	p->timeofdeath = time_generate(death_ref, var->start_time, var);
 }
