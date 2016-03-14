60b94db27b3b865a04a4ca7868953f1a0a3738ce
Julien LINDEN <jlinden@student.42.fr>
2016-03-14 19:10:01 +0100

    Check if action exists in update_tick
    Quick fix, might hide a bigger issue.

--- src/time_compute.c
+++ src/time_compute.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include "serveur.h"
 
-void		compute_action_new_time(t_action *action, int old_tick ,t_zappy *var)
+void		compute_action_new_time(t_action *action, int old_tick, t_zappy *var)
 {
 	double	action_ref;
 
+	if (!action)
+		return ;
 	action_ref = time_elapsed(var->start_time, action->trigger_t) / 1000000 * old_tick;
 	action->trigger_t = time_generate(action_ref, var->start_time, var);
 	action->creation_t = var->start_time;
@@ -15,6 +17,8 @@ void		compute_death_new_time(t_player *p, int old_tick, t_zappy *var)
 {
 	double	death_ref;
 
+	if (!p)
+		return ;
 	death_ref = time_elapsed(var->start_time, p->timeofdeath) / 1000000 * old_tick;
 	p->timeofdeath = time_generate(death_ref, var->start_time, var);
 }
