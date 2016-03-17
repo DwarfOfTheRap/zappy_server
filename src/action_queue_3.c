a6db678245a8205bffac2593e01b15bca4d04d0e
Julien LINDEN <jlinden@student.42.fr>
2016-03-17 22:23:21 +0100

    Search player's last action from the end

--- src/action_queue_3.c
+++ src/action_queue_3.c
@@ -27,23 +27,19 @@ t_action	*find_player_last_action(t_player *p, t_zappy *var)
 	t_lst_head	*list;
 	t_lst_elem	*cursor;
 	t_action	*action;
-	t_action	*final_action;
 
-	// TODO:
-	// CHECK PENDING_ACTIONS DURING LOOP TO STOP ITERATING IF WE ALREADY FOUND IT
 	if (!(p && var) || !p->pending_actions)
 		return (NULL);
 	list = &var->actions;
-	final_action = NULL;
 	action = NULL;
-	cursor = list->first;
+	cursor = list->last;
 	while (cursor)
 	{
 		if ((action = (t_action*)cursor->content) && action->player == p)
-			final_action = action;
+			return (action);
 		cursor = cursor->next;
 	}
-	return (final_action);
+	return (NULL);
 }
 
 t_action	*find_player_first_action(t_player *p, t_zappy *var)
