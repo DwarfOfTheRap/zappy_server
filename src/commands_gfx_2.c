6707b55555bead903f90b381110f46611fb83340
Marc DELAGE <mdelage@student.42.fr>
2016-03-19 15:34:21 +0100

    fix gfx player command error due to unique id

--- src/commands_gfx_2.c
+++ src/commands_gfx_2.c
@@ -20,6 +20,18 @@ void	command_sst(t_zappy *var, t_player *p, char *arg)
 		log_gfx_str("sst", arg);
 }
 
+static int	get_player_from_unique_id(t_zappy *var, int unique_id)
+{
+	int		i;
+
+	i = 4;
+	while (i <= *var->fd_max && unique_id != var->players[i].unique_id)
+		++i;
+	if (i <= *var->fd_max && unique_id == var->players[i].unique_id)
+		return (i);
+	return (0);
+}
+
 void	command_ppo(t_zappy *var, t_player *p, char *arg)
 {
 	int		p_id;
@@ -29,6 +41,7 @@ void	command_ppo(t_zappy *var, t_player *p, char *arg)
 	p_id = (int)strtol(arg, &end, 10);
 	if (end == arg)
 		return (message_gfx_sbp(var));
+	p_id = get_player_from_unique_id(var, p_id);
 	if (p_id <= 0 || MAX_FD < p_id || var->players[p_id].status != FD_CLIENT)
 		return (message_gfx_sbp(var));
 	message_gfx_ppo(var, &var->players[p_id]);
@@ -45,6 +58,7 @@ void	command_plv(t_zappy *var, t_player *p, char *arg)
 	p_id = (int)strtol(arg, &end, 10);
 	if (end == arg)
 		return (message_gfx_sbp(var));
+	p_id = get_player_from_unique_id(var, p_id);
 	if (p_id <= 0 || MAX_FD < p_id || var->players[p_id].status != FD_CLIENT)
 		return (message_gfx_sbp(var));
 	message_gfx_plv(var, &var->players[p_id]);
@@ -61,6 +75,7 @@ void	command_pin(t_zappy *var, t_player *p, char *arg)
 	p_id = (int)strtol(arg, &end, 10);
 	if (end == arg)
 		return (message_gfx_sbp(var));
+	p_id = get_player_from_unique_id(var, p_id);
 	if (p_id <= 0 || MAX_FD < p_id || var->players[p_id].status != FD_CLIENT)
 		return (message_gfx_sbp(var));
 	message_gfx_pin(var, &var->players[p_id]);
