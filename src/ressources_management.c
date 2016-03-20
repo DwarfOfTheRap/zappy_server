9091e54d157e7303c4e735a626fb1bcd6c5d278b
Marc DELAGE <mdelage@student.42.fr>
2016-03-20 19:05:06 +0100

    ressource quantity proportionnal to number of team

--- src/ressources_management.c
+++ src/ressources_management.c
@@ -25,12 +25,18 @@ void	init_ressources(t_zappy *var)
 
 	nb_square = var->board_size[0] * var->board_size[1];
 	add_ressource(var, 0, nb_square * FOOD_RATIO / 100);
-	add_ressource(var, 1, nb_square * RES_RATIO / 100 * 9 / 39);
-	add_ressource(var, 2, nb_square * RES_RATIO / 100 * 8 / 39);
-	add_ressource(var, 3, nb_square * RES_RATIO / 100 * 10 / 39);
-	add_ressource(var, 4, nb_square * RES_RATIO / 100 * 5 / 39);
-	add_ressource(var, 5, nb_square * RES_RATIO / 100 * 6 / 39);
-	add_ressource(var, 6, nb_square * RES_RATIO / 100 / 39);
+	add_ressource(var, 1, nb_square * RES_RATIO * (1 + var->nb_team / 2) /
+			100 * 9 / 39);
+	add_ressource(var, 2, nb_square * RES_RATIO * (1 + var->nb_team / 2) /
+			100 * 8 / 39);
+	add_ressource(var, 3, nb_square * RES_RATIO * (1 + var->nb_team / 2) /
+			100 * 10 / 39);
+	add_ressource(var, 4, nb_square * RES_RATIO * (1 + var->nb_team / 2) /
+			100 * 5 / 39);
+	add_ressource(var, 5, nb_square * RES_RATIO * (1 + var->nb_team / 2) /
+			100 * 6 / 39);
+	add_ressource(var, 6, nb_square * RES_RATIO * (1 + var->nb_team / 2) /
+			100 / 39);
 }
 
 void	dispatch_incantation_ressources(t_zappy *var, t_player *p,
