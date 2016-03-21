c5ff234ebe67111a69fb584e45099b075a37f442
Marc DELAGE <mdelage@student.42.fr>
2016-03-21 20:16:59 +0100

    correct some norme error

--- src/connexion_2.c
+++ src/connexion_2.c
@@ -45,9 +45,7 @@ void			init_gfx(t_zappy *var, t_player *p)
 		p->status = FD_GFX;
 		--p->team->remain;
 		var->gfx_client = p;
-		// save the current time to reverbate it on action queue
 		init_gfx_sub(var);
-		// iter on action queue to update tmstmp
 	}
 	else
 	{
